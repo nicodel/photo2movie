97f7ee4cf5d8804f7a73b69a8ea4ce61f700800e
nicodel <nicolas.delebecque@gmail.com>
2014-11-22 13:54:50 +0100

    rework

--- js/app.js
+++ js/app.js
@@ -1,2 +1,8 @@
 "use strict"
-var p2m = function() {}();
+var p2m = function() {
+  document.addEventListener('DOMComponentsLoaded', function(){
+    document.querySelector('x-deck').showCard(2);
+    Controller.init()
+  });
+ 
+}();
--- js/controller.js
+++ js/controller.js
@@ -1,2 +1,39 @@
 "use strict"
-var Controller = function() {}();
+var Controller = function() {
+  var video = document.querySelector('#video'),
+      cover = document.querySelector('#cover'),
+      canvas = document.querySelector('#canvas'),
+      vidcontainer = document.querySelector('#videocontainer');
+
+ function init() {
+    navigator.getMedia = ( navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia);
+    navigator.getMedia(
+      {
+        video: true,
+        audio: false
+      },
+      function(stream) {
+        if (navigator.mozGetUserMedia) {
+          video.mozSrcObject = stream;
+        } else {
+          var vendorURL = window.URL || window.webkitURL;
+          video.src = vendorURL ? vendorURL.createObjectURL(stream) : stream;
+        }
+      video.play();
+      video.style.width = width + 'px';
+      video.style.height = height + 'px';
+      },
+      function(err) {
+        console.log("An error occured! " + err);
+      }
+    );
+  }
+
+  return {
+    init: init
+  };
+
+}();
