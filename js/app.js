735f172f456914b62d758bc99ed2087f65c762f9
nicodel <nicolas.delebecque@gmail.com>
2014-11-22 23:15:46 +0100

    taking photos and displaying it behind video feed works again

--- js/app.js
+++ js/app.js
@@ -6,26 +6,25 @@ var p2m = function() {
   });
   var streaming = false,
       video        = document.querySelector('#video'),
-      player       = document.querySelector('#player'),
-      cover        = document.querySelector('#cover'),
       canvas       = document.querySelector('#canvas'),
       photo        = document.querySelector('#photo'),
       startbutton  = document.querySelector('#startbutton'),
-      width        = 320,
-      height       = 0;
+      width = 320,
+      // height = width * 0.75;
+      height = 240;
 
-  navigator.getMedia = ( navigator.getUserMedia || 
+  navigator.getMedia = ( navigator.getUserMedia ||
                          navigator.webkitGetUserMedia ||
                          navigator.mozGetUserMedia ||
                          navigator.msGetUserMedia);
 
   navigator.getMedia(
-    { 
-      video: true, 
-      audio: false 
+    {
+      video: true,
+      audio: false
     },
     function(stream) {
-      if (navigator.mozGetUserMedia) { 
+      if (navigator.mozGetUserMedia) {
         video.mozSrcObject = stream;
       } else {
         var vendorURL = window.URL || window.webkitURL;
@@ -40,30 +39,32 @@ var p2m = function() {
 
   video.addEventListener('canplay', function(ev){
     if (!streaming) {
-      height = video.videoHeight / (video.videoWidth / width);
+      // height = video.videoHeight / (video.videoWidth/width);
+      // width = video.videoWidth;
+      // height = video.videoHeight;
+      width = 320;
+      height = 240;
       video.setAttribute('width', width);
       video.setAttribute('height', height);
-      canvas.setAttribute('width', width);
-      canvas.setAttribute('height', height);
+      // canvas.setAttribute('width', width);
+      // canvas.setAttribute('height', height);
+      console.log("width x height", width + " x " + height);
       streaming = true;
     }
   }, false);
 
   function takepicture() {
-    
-    var h = video.videoHeight / (video.videoWidth / width);
     canvas.width = width;
-    canvas.height = h;
-    canvas.getContext('2d').drawImage(video, 0, 0, width, h);
-    var data = canvas.toDataURL('image/png');
-    console.log('data', data);
+    canvas.height = height;
+    canvas.getContext('2d').drawImage(video, 0, 0, width, height);
+    var data = canvas.toDataURL();
+    console.log("data", data);
     photo.setAttribute('src', data);
-    // encoder.add(canvas.getContext("2d"));
+    video.className = "transparent";
   }
 
   startbutton.addEventListener('click', function(ev){
-  	takepicture();
+      takepicture();
     ev.preventDefault();
   }, false);
-
 }();
