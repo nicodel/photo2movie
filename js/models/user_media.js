5f658b2d674c73071db9c03bcdaf4165c93dbd7d
nicodel <nicolas.delebecque@gmail.com>
2015-03-01 01:14:19 +0100

    cleaning

--- js/models/user_media.js
+++ js/models/user_media.js
@@ -7,50 +7,27 @@ var UserMedia = function(elements) {
   var vendorURL;
   var streaming = false;
 
-  // var width = "320px";  //Flame
-  // var height = "539px"; //Flame
   var width = window.innerWidth;
   var height = window.innerHeight;
+  console.log("sizes: w x h", width + " x " + height);
 
   var ev_photo_taken = new Event(this);
   var ev_error = new Event(this);
 
   var video = elements.video_container;
-  // var photo = elements.photo_container;
   var canvas = elements.canvas;
-  // var take_btn = elements.take_btn;
 
-  // take_btn.addEventListener("click", function() {
   var takePhoto = function() {
     canvas.width = width;
     canvas.height = height;
     canvas.getContext("2d").drawImage(video, 0, 0, width, height);
     var data = canvas.toDataURL();
-    // console.log("data", data);
     ev_photo_taken.notify(data);
-/*    photo.setAttribute("src", data);
-    video.className = "transparent";*/
-    // current_anim = Animations.addPhoto([nb, data]);
-    // DB.updateAnimation(__updateAnimationSuccess, __updateAnimationError, current_anim);
   };
-  // });
-
-/*  video.addEventListener("loadeddata", function() {
-    var camera_w = video.videoWidth;
-    var camera_h = video.videoHeight;
-    console.log("Camera", camera_w + " x " + camera_h);
-    var screen_w = screen.width;
-    var screen_h = screen.height;
-    // width = screen.videoWidth;
-    // height = screen.videoHeight;
-    console.log("Screen", screen_w + " x " + screen_h);
-  }, false);*/
 
   video.addEventListener("canplay", function(){
     if (!streaming) {
       video.setAttribute("height", height);
-      // video.setAttribute("width", width);
-      // console.log("width x height", width + " x " + height);
       streaming = true;
     }
   }, false);
